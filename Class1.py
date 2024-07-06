c949bca378abd773069fed818f2e9c1045d1308b
Marina Cantarelli <macbarroca@gmail.com>
2024-07-05 21:56:22 -0300

    Meu primeiro codigo em Python!

---
+++ Class1.py
@@ -0,0 +1,10 @@
+#Primeira aula de programação em python!! :)
+print("Ola")
+print('Tudo bem?')
+print('Que dia é hoje?') #Em aspas simples o python entende que é texto. 
+#Ele copia e cola n console o texto. 
+
+#Comentario de multiplas linhas são feitos com ''' '''
+'''
+print('Hello world!')
+'''
