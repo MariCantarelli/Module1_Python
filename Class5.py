a73fe84f1c4b5a5957c44ee985e8d08a517ff450
Marina Cantarelli <macbarroca@gmail.com>
2024-07-07 17:55:07 -0300

    Working with Input

---
+++ Class5.py
@@ -0,0 +1,8 @@
+#Working with Input
+
+Name = input('What is your name: ')
+Age = input('What is your age: ')
+Age = str(Age) #is a way to change Integer do String before the print 
+City = input('Where do you live: ')
+
+print(Name + " is " + Age + " years old and lives in the city of " + City + ".")
