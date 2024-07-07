d216cffac91c15b662ee73d515d9d13fa524161f
Marina Cantarelli <macbarroca@gmail.com>
2024-07-07 18:53:02 -0300

    Calculate age with Input

---
+++ Class6.py
@@ -0,0 +1,7 @@
+#Calculate age with Input
+
+year_birth = input('What year were you born?') 
+#print(type(year_birth)) Explains the data type 
+age = 2024 - int(year_birth) #Transform string in int, because this is a erro in Python on the output
+
+print(age)
