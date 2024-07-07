fcf2b1326e625cbcaf53e871714e2cdfa0ad6011
Marina Cantarelli <macbarroca@gmail.com>
2024-07-07 16:30:59 -0300

    Data manipulation

---
+++ Class3.py
@@ -0,0 +1,19 @@
+#data manipulation
+
+
+'''x = 3 
+    y = 4
+    z = 5
+
+    print(x)
+    print(y)
+    print(z)
+
+'''
+x = str(3) #insted of integer, 3 will be used as string
+y = int (4) #will always be used as integer now
+z = float(5) #will always be used as float now
+
+print(x + x)
+print(y + y)
+print(z + z)
