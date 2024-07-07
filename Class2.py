5acaeaee259c719394fe2008993080c9618a5f1f
Marina Cantarelli <macbarroca@gmail.com>
2024-07-07 16:03:43 -0300

    Learing how to use variable in Pyhton

---
+++ Class2.py
@@ -0,0 +1,14 @@
+#learning how to use variables in Pyhton! 
+
+'''x = 8
+print(x + x)
+print(x + x)
+print(x + x)
+print(x + x)
+
+x = "Hi!" #or use 'Hi! 
+print(x)'''
+
+x = 2.4
+print(x)
+print(x + x)
