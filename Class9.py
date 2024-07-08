8457fb23921f8003880560736e09971121c66303
Marina Cantarelli <macbarroca@gmail.com>
2024-07-07 22:52:09 -0300

    Methods for String

---
+++ Class9.py
@@ -0,0 +1,13 @@
+#Methods for String
+Mensage = ' I love homemade Food'
+
+print(Mensage.lower())#tudo em letra minuscula
+print(Mensage.upper())#tudo em letra maiuscula
+print(Mensage.capitalize())# 1 letra mauiscula na string
+print(Mensage.find('h')) #Find the h on the index, and tell the index
+print(Mensage.find('love'))
+print(Mensage.find('loVe')) # Se o output der -1, ele não encontrou um resultado igual na String
+print(Mensage.replace('l', "L")) # Troca o 1 pelo segundo caractere
+print(Mensage.replace('love', "hate")) #Troca o 1 pelo segunda palavra 
+print(Mensage.strip()) # Retira os espaços antes do 1 caractere
+print(Mensage.find('h'))
