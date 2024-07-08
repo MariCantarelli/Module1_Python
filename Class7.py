49229a8135681db8aefe46fd3461f657132b06c9
Marina Cantarelli <macbarroca@gmail.com>
2024-07-07 21:06:33 -0300

    Slice

---
+++ Class7.py
@@ -0,0 +1,14 @@
+#Slice
+
+fruit = 'avocato'
+#index   0123456
+print(fruit[2:6]) # from the 2 character to the 5 character
+print(fruit[-1]) # from the last character
+print(fruit[4]) 
+
+valor = 99.75
+valor = str(valor)
+#index  01234
+print(valor[3:5]) # Only works with String!!!
+print(valor[2:]) # The 2 last character 
+print(valor[:2]) # The first 2 character
